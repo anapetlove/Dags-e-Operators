74482c19faabab7a13ff9314b3b978a878d37a63
anapetlove <51921188+anapetlove@users.noreply.github.com>
2019-07-02 18:06:53 -0300

    Add files via upload

---
+++ pig_dag.py
@@ -0,0 +1,53 @@
+import airflow
+import pandas as pd
+import psycopg2
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.postgres_plugin_operator import PostgresToS3
+from airflow.contrib.operators.aws_athena_operator import AWSAthenaOperator
+from templates.pig_templates import TEMPLATES
+import airflow.macros
+
+default_args = {
+    'owner': 'airflow',    
+    'start_date': airflow.utils.dates.days_ago(0),
+    'depends_on_past': False
+}
+
+partition = "pyear={{macros.datetime.strptime(ds_nodash, '%Y%m%d').strftime('%Y')}}/pmonth={{macros.datetime.strptime(ds_nodash, '%Y%m%d').strftime('%m')}}/pday={{macros.datetime.strptime(ds_nodash, '%Y%m%d').strftime('%d')}}/prefdate={{ds_nodash}}/"
+
+with DAG(dag_id='passing_pig', default_args=default_args, schedule_interval=timedelta(days=1)) as dag:
+   for template in TEMPLATES:
+        send_s3_raw = PostgresToS3(task_id='send_s3_raw_'+template,
+                                sql='/templates/pig_'+template+'.sql',
+                                file_name=template+'_{{ds_nodash}}_{{ts_nodash}}.parquet',
+                                postgres_conn_id='postgres_conn_id',
+                                aws_conn_id='aws_petlove',
+                                s3_bucket='petlove-nessie-dev',
+                                s3_key='ingestion_tier/raw_data_zone/pig/pig_'+template+'/'+partition
+        )
+
+        repair_raw = AWSAthenaOperator(task_id='repair_raw_'+template,
+                                query='MSCK REPAIR TABLE pig_'+template,
+                                database='raw',
+                                output_location='s3://aws-athena-query-results-381158256258-us-east-2/',
+                                aws_conn_id='aws_petlove'
+        )
+
+        #send_s3_trusted = BashOperator(task_id='send_s3_trusted',
+         #               bash_command= 'aws s3 cp s3://petlove-nessie-dev/ingestion_tier/raw_data_zone/pig/pig_'+template+'/'+partition
+        #)
+        #repair_trusted_Athena = AWSAthenaOperator(task_id='repair_trusted_'+template,
+        #                query='MSCK REPAIR TABLE pig_'+template,
+        #                database='trusted',
+        #                output_location='s3://aws-athena-query-results-381158256258-us-east-2/',
+        #                aws_conn_id='aws_petlove'
+        #)
+        send_s3_raw >> repair_raw
+
+        
+
+
+
+
+ 
